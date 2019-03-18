b7dcd1ccf8c675d14512e4138f2f8330e6db6bab
Ajil Oommen <ajilo297@gmail.com>
2019-03-18 20:55:34 +0530

    Added Counter bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,98 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  final CounterBloc _counterBloc = CounterBloc();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Bloc Demo',
+      home: BlocProvider(
+        bloc: _counterBloc,
+        child: CounterPage(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  void dispose() {
+    _counterBloc.dispose();
+    super.dispose();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class CounterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final CounterBloc _counterBloc = BlocProvider.of<CounterBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Counter'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: BlocBuilder<CounterEvent, int>(
+        bloc: _counterBloc,
+        builder: (BuildContext context, int count) {
+          return Center(
+            child: Text(
+              '$count',
+              style: TextStyle(fontSize: 24.0),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          _floatingActionButton(
+            onPressed: () => _counterBloc.dispatch(CounterEvent.increment),
+            child: Icon(Icons.add),
+          ),
+          _floatingActionButton(
+            onPressed: () => _counterBloc.dispatch(CounterEvent.decrement),
+            child: Icon(Icons.remove),
+          ),
+        ],
       ),
     );
+  }
+
+  Widget _floatingActionButton(
+      {@required VoidCallback onPressed, @override Widget child}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: FloatingActionButton(
+        onPressed: onPressed,
+        child: child,
+      ),
+    );
+  }
+}
+
+enum CounterEvent { increment, decrement }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  @override
+  int get initialState => 0;
+
+  @override
+  Stream<int> mapEventToState(int currentState, CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.increment:
+        yield currentState += 1;
+        break;
+      case CounterEvent.decrement:
+        yield currentState -= 1;
+        break;
+    }
   }
 }
